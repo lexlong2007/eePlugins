f04a3d4e485d840ccd408f331769fe21d3bd2583
Zdzislaw <zdzislaw22@windowslive.com>
2017-11-09 13:01:23 +0100

    BH add additional fonts

--- BH/usr/lib/enigma2/python/Components/Converter/BlackHarmonyBitrate_BH.py
+++ BH/usr/lib/enigma2/python/Components/Converter/BlackHarmonyBitrate_BH.py
@@ -15,11 +15,16 @@ class BlackHarmonyBitrate_BH(Converter, object):
         self.container.dataAvail.append(self.dataAvail)
         self.timer = eTimer()
         self.timer.callback.append(self.start)
+        self.runTimer = eTimer()
+        self.runTimer.callback.append(self.runBitrate)
 
     @cached
     def getText(self):
         #print "bitrate class getText", self.vcur
-        return '%d Kb/s' % self.vcur
+        if self.vcur > 0:
+            return '%d Kb/s' % self.vcur
+        else:
+            return ''
 
     text = property(getText)
 
@@ -29,35 +34,53 @@ class BlackHarmonyBitrate_BH(Converter, object):
             self.running = 1
             self.start()
         else:
-            self.container.kill()
+            os.system('killall -9 bitrate > /dev/null 2>&1') #don't leave garbage (defunct process)
+            #self.container.kill()
             self.clearValues()
 
     def start(self):
         if self.source.service and self.running == 1:
-            demux = 2
-            try:
-                stream = self.source.service.stream()
-                if stream:
-                    streamdata = stream.getStreamingData()
-                    if streamdata and 'demux' in streamdata:
-                        demux = streamdata['demux']
-            except:
-                pass
-            info = self.source.service.info()
-            vpid = info.getInfo(iServiceInformation.sVideoPID)
-            apid = info.getInfo(iServiceInformation.sAudioPID)
-            if vpid >= 0 and apid >= 0:
-                cmd = 'bitrate %i %i %i' % ( demux, vpid, vpid )
-                print "bitrate class start", cmd
-                self.running = 2
-                os.system('killall -9 bitrate > /dev/null 2>&1')
-                self.container.execute(cmd)
+            self.runTimer.start(600, True)
         else:
-           print "bitrate class start po else", self.source.service, self.running
+            print "bitrate class start po else", self.source.service, self.running
         if self.running == 1:
+            os.system('echo "self.timer.start(300, True)" >> /tmp/bitrate')
             #print "bitrate class setup timer"
             self.timer.start(300, True)
 
+    def runBitrate(self):
+        os.system('killall -9 bitrate > /dev/null 2>&1')
+        if os.path.exists('/usr/lib/enigma2/python/Plugins/SystemPlugins/VTIPanel'):
+            demux = 2
+        else:
+            adapter = 0
+            demux = 0
+        try:
+            stream = self.source.service.stream()
+            if stream:
+                streamdata = stream.getStreamingData()
+                if streamdata:
+                    if 'demux' in streamdata:
+                        demux = streamdata['demux']
+                        if demux < 0: demux = 0
+                    if 'adapter' in streamdata:
+                        adapter = streamdata["adapter"]
+                        if adapter < 0: adapter = 0
+        except:
+            pass
+        info = self.source.service.info()
+        vpid = info.getInfo(iServiceInformation.sVideoPID)
+        apid = info.getInfo(iServiceInformation.sAudioPID)
+        os.system('echo "bitrate %i %i %i %i" >> /tmp/bitrate' % ( adapter, demux, vpid, vpid ))
+        if vpid >= 0 and apid >= 0:
+            if os.path.exists('/usr/lib/enigma2/python/Plugins/SystemPlugins/VTIPanel'):
+                cmd = 'bitrate %i %i %i' % ( demux, vpid, vpid )
+            else:
+                cmd = 'bitrate %i %i %i %i' % ( adapter, demux, vpid, vpid )
+            print "bitrate class start", cmd
+            self.running = 2
+            self.container.execute(cmd)
+    
     def clearValues(self):
         self.vmin = self.vmax = self.vavg = self.vcur = 0
         self.amin = self.amax = self.aavg = self.acur = 0
