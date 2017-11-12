e41d4b41fb52d7fbd5c7347b1641db6570cce5ca
Zdzislaw <zdzislaw22@windowslive.com>
2017-11-12 10:59:48 +0100

    BH temp on arm boxes

--- BH/usr/lib/enigma2/python/Components/Converter/BlackHarmonyFanTempInfo.py
+++ BH/usr/lib/enigma2/python/Components/Converter/BlackHarmonyFanTempInfo.py
@@ -1,18 +1,4 @@
-# BlackHarmonyFanTempInfo Converter  v.0.3
-# Copyright (c) 2boom 2012-14
-# v.0.3-r0
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# BlackHarmonyFanTempInfo Converter  v.0.4
 
 from Poll import Poll
 from Components.Converter.Converter import Converter
@@ -20,33 +6,57 @@ from Components.Element import cached
 from Tools.Directories import fileExists
 
 class BlackHarmonyFanTempInfo(Poll, Converter, object):
-	FanInfo = 0
-	TempInfo = 1
-	
-	def __init__(self, type):
-		Converter.__init__(self, type)
-		Poll.__init__(self)
-		if type == "FanInfo":
-			self.type = self.FanInfo
-		elif type == "TempInfo":
-			self.type = self.TempInfo
-		self.poll_interval = 2000
-		self.poll_enabled = True
-	
-	@cached
-	
-	def getText(self):
-		info = 'N/A'
-		if self.type == self.FanInfo:
-			if fileExists("/proc/stb/fp/fan_speed"):
-				info = open("/proc/stb/fp/fan_speed").read().strip('\n')
-		elif self.type == self.TempInfo:
-			if fileExists("/proc/stb/sensors/temp0/value") and fileExists("/proc/stb/sensors/temp0/unit"):
-				info = "%s%s%s" % (open("/proc/stb/sensors/temp0/value").read().strip('\n'), unichr(176).encode("latin-1"), open("/proc/stb/sensors/temp0/unit").read().strip('\n'))
-		return info
-	
-	text = property(getText)
-	
-	def changed(self, what):
-		if what[0] == self.CHANGED_POLL:
-			self.downstream_elements.changed(what)
+    FanInfo = 0
+    TempInfo = 1
+    TempInfoHeader = 3
+    FanInfoHeader = 4
+    
+    def __init__(self, type):
+        Converter.__init__(self, type)
+        Poll.__init__(self)
+        if type == "FanInfo":
+            self.type = self.FanInfo
+        elif type == "TempInfo":
+            self.type = self.TempInfo
+        self.poll_interval = 2000
+        self.poll_enabled = True
+        if fileExists("/proc/stb/fp/fan_speed"):
+            self.FanHeader = 'FAN:'
+        else:
+            self.FanHeader = ''
+            
+        self.TempHeader = ''
+        self.TempUnit = ''
+        if fileExists("/proc/stb/sensors/temp0/value") and fileExists("/proc/stb/sensors/temp0/unit"):
+            self.TempPath = "/proc/stb/sensors/temp0/value"
+            self.TempHeader = 'TMP:'
+            self.TempUnit = open("/proc/stb/sensors/temp0/unit").read().strip('\n')
+        elif fileExists('/sys/devices/virtual/thermal/thermal_zone0/temp'):
+            try:
+                open("/sys/devices/virtual/thermal/thermal_zone0/temp").read() #check if it can be read, onsolo4k it doesn't
+                self.TempPath = "/proc/stb/sensors/temp0/value"
+                self.TempHeader = 'TMP:'
+                self.TempUnit = 'C'
+            except Exception:
+                pass
+    
+    @cached
+    def getText(self):
+        try:
+            if self.type == self.FanInfoHeader:
+                    return self.FanHeader
+            elif self.type == self.FanInfo and self.FanHeader != '':
+                return open("/proc/stb/fp/fan_speed").read().strip('\n')
+            elif self.type == self.TempInfoHeader:
+                    return self.TempHeader
+            elif self.type == self.TempInfo and self.TempHeader != '':
+                return "%s%s%s" % (open().read(self.TempPath).strip('\n')[:2], unichr(176).encode("latin-1"), self.TempUnit )
+        except Exception:
+            pass
+        return ''
+    
+    text = property(getText)
+    
+    def changed(self, what):
+        if what[0] == self.CHANGED_POLL:
+            self.downstream_elements.changed(what)
