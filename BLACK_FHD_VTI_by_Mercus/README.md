7e44fef2e36e1bc1fa6c1c8959977b95255540fb
Zdzislaw <zdzislaw22@windowslive.com>
2018-02-04 00:24:24 +0100

    BLACK FHD dla VTI autor Mercus v. 1.2

---
+++ BLACK_FHD_VTI_by_Mercus/README.md
@@ -0,0 +1,1 @@
+# skin_Black_FHD_VTI by Mercus
---
+++ BLACK_FHD_VTI_by_Mercus/usr/lib/enigma2/python/Components/Converter/BlackHarmonyVersionConverter.py
@@ -0,0 +1,34 @@
+#
+# http://blackharmony.pl/
+# 2015-11-19 areq
+#
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+from socket import gethostbyname
+from time import time
+
+class BlackHarmonyVersionConverter(Converter, object):
+    VERSION = 43
+    def __init__(self, arg):
+        Converter.__init__(self, arg)
+        self.toupdate = False
+        self.nextt = 0
+
+    @cached
+    def getBoolean(self):
+        t = time()
+        v = 0
+        if self.nextt < t:
+            self.nextt = t + (60 * 10)
+            try:
+                i1, i2, i3, i4 = [ int(i) for i in gethostbyname('version.blackharmony.pl').split('.') ]
+                if i1 == 1:
+                    v = i3 * 256 + i4
+                    self.nextt = t + 14400   # every 4h
+            except:
+                pass
+            self.toupdate =  self.VERSION < v
+
+        return self.toupdate
+
+    boolean = property(getBoolean)
