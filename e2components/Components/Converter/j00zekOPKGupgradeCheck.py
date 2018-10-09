2b8bcf40c033728e5af32ab6fc48fd8c74cad279
Zdzislaw <zdzislaw22@windowslive.com>
2018-10-09 09:35:35 +0200

    poprawka e2c

--- e2components/Components/Converter/j00zekOPKGupgradeCheck.py
+++ e2components/Components/Converter/j00zekOPKGupgradeCheck.py
@@ -11,38 +11,41 @@ if DBG: from Components.j00zekComponents import j00zekDEBUG
 class j00zekOPKGupgradeCheck(Converter, object):
     def __init__(self, arg):
         Converter.__init__(self, arg)
-        self.lastState = False
+        if DBG: j00zekDEBUG('[j00zekOPKGupgradeCheck:__init__] >>>')
         self.currState = False
+        self.retstr = ''
         self.container = eConsoleAppContainer()
         self.container.appClosed.append(self.appClosed)
         self.container.dataAvail.append(self.dataAvail)
         self.checkTimer = eTimer()
         self.checkTimer.callback.append(self.checkOPKG)
-        self.checkTimer.start(86400000) #check once a day = 1000ms * 60 * 60 * 24
+        if DBG: self.checkTimer.start(60000) #check once a minute for tests
+        else: self.checkTimer.start(86400000) #check once a day 86400000 = 1000ms * 60 * 60 * 24
 
     @cached
     def getBoolean(self):
-        if DBG: j00zekDEBUG('[BlackHarmonyVersionConverter:getBoolean] self.lastState=%s' % self.lastState)
-        return self.lastState
+        if DBG: j00zekDEBUG('[j00zekOPKGupgradeCheck:getBoolean] self.currState=%s' % self.currState)
+        return self.currState
 
     boolean = property(getBoolean)
     
     def checkOPKG(self):
-        if DBG: j00zekDEBUG('[BlackHarmonyVersionConverter:checkOPKG] >>>')
+        if DBG: j00zekDEBUG('[j00zekOPKGupgradeCheck:checkOPKG] >>>')
+        self.retstr = ''
         cmd=[]
-        cmd.append('opkg update')
+        cmd.append('opkg update > /dev/null')
         cmd.append('opkg list-upgradable')
         self.container.execute(";".join(cmd))
     
     def appClosed(self, retval):
-        if DBG: j00zekDEBUG("[BlackHarmonyVersionConverter:appClosed] retval=%s" % str(retval))
-        if self.lastState != self.currState:
-            self.lastState = self.currState
-            Converter.changed(self, (self.CHANGED_POLL,))
-
-    def dataAvail(self, str):
-        if DBG: j00zekDEBUG("[BlackHarmonyVersionConverter:dataAvail] %s" % str)
-        if str.find('j00zek') > 0:
+        if DBG: j00zekDEBUG("[j00zekOPKGupgradeCheck:appClosed] retval=%s" % str(retval))
+        if self.retstr.find('j00zek') > 0:
             self.currState = True
         else:
             self.currState = False
+        self.retstr = ''
+        if DBG: j00zekDEBUG("[j00zekOPKGupgradeCheck:dataAvail] self.currState=%s" % self.currState)
+
+    def dataAvail(self, str):
+        if DBG: j00zekDEBUG("[j00zekOPKGupgradeCheck:dataAvail] %s" % str)
+        self.retstr += str 
