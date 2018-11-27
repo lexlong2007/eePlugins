07df742d7a64e02cca27139c07e606e59ab286a1
Zdzislaw <zdzislaw22@windowslive.com>
2018-11-27 13:36:37 +0100

    aktualizacja BH

--- BH/usr/lib/enigma2/python/Components/BlackHarmonyWeatherMSN.py
+++ BH/usr/lib/enigma2/python/Components/BlackHarmonyWeatherMSN.py
@@ -1,73 +1,24 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 #
-# WeatherPlugin E2
-#
-# j00zek: this file has changed name just to avoid errors using opkg (situation when file was installed by different pockage)
-#
-# Coded by Dr.Best (c) 2012-2013
-# Support: www.dreambox-tools.info
-# E-Mail: dr.best@dreambox-tools.info
-#
-# This plugin is open source but it is NOT free software.
-#
-# This plugin may only be distributed to and executed on hardware which
-# is licensed by Dream Multimedia GmbH.
-# In other words:
-# It's NOT allowed to distribute any parts of this plugin or its source code in ANY way
-# to hardware which is NOT licensed by Dream Multimedia GmbH.
-# It's NOT allowed to execute this plugin and its source code or even parts of it in ANY way
-# on hardware which is NOT licensed by Dream Multimedia GmbH.
-#
-# If you want to use or modify the code or parts of it,
-# you have to keep MY license and inform me about the modifications by mail.
+# j00zek: this file is just to avoid GS-es, when weather plugin not installed
 #
 
-from enigma import eEnv, eTimer
-
-class fakeMSNWaether:
+class fakeComponentsMSNWaether:
     def  __init__(self):
         pass
       
     def OK(self):
-        return False
-      
-try:
-    from Plugins.Extensions.WeatherPlugin.MSNWeather import MSNWeather
-except Exception:
-    MSNWeather = fakeMSNWaether
-    
-
-class BlackHarmonyWeatherMSN:
-    TIMER_INTERVAL = 1800
+        return False 
+        
+class FakeWeatherMSN:
     def __init__(self):
-        self.weatherData = MSNWeather()
+        self.weatherData = fakeComponentsMSNWaether()
         self.callbacks = [ ]
         self.callbacksAllIconsDownloaded = []
-        self.timer = eTimer()
-        self.timer.callback.append(self.getData)
-    
-    def getData(self):
-        self.timer.stop()
-        self.weatherData.getDefaultWeatherData(self.callback, self.callbackAllIconsDownloaded)
-        self.timer.startLongTimer(self.TIMER_INTERVAL)
-        
-    def updateWeather(self, weather, result, errortext):
-        if result == MSNWeather.OK:
-            self.timer.stop()
-            self.weatherData = weather
-            self.weatherData.callback = None
-            self.weatherData.callbackShowIcon = None
-            self.weatherData.callbackAllIconsDownloaded = None
-            self.callback(result, errortext)
-            self.callbackAllIconsDownloaded()
-            self.timer.startLongTimer(self.TIMER_INTERVAL)
 
-    def callbackAllIconsDownloaded(self):
-        for x in self.callbacksAllIconsDownloaded:
-            x()
+try:
+    from Components.WeatherMSN import WeatherMSN
+    weathermsn = WeatherMSN()
+except Exception:
+    weathermsn = FakeWeatherMSN()
 
-    def callback(self, result, errortext):
-        for x in self.callbacks:
-            x(result, errortext)
-    
-weathermsn = BlackHarmonyWeatherMSN()
--- BH/usr/lib/enigma2/python/Components/Sources/BlackHarmonyMSNWeather.py
+++ BH/usr/lib/enigma2/python/Components/Sources/BlackHarmonyMSNWeather.py
@@ -1,176 +1,36 @@
 # -*- coding: utf-8 -*-
 #
-# WeatherPlugin E2
-#
-# j00zek: this file has changed name just to avoid errors using opkg (situation when file was installed by different pockage)
-#
-# Coded by Dr.Best (c) 2012-2013
-# Support: www.dreambox-tools.info
-# E-Mail: dr.best@dreambox-tools.info
-#
-# This plugin is open source but it is NOT free software.
-#
-# This plugin may only be distributed to and executed on hardware which
-# is licensed by Dream Multimedia GmbH.
-# In other words:
-# It's NOT allowed to distribute any parts of this plugin or its source code in ANY way
-# to hardware which is NOT licensed by Dream Multimedia GmbH.
-# It's NOT allowed to execute this plugin and its source code or even parts of it in ANY way
-# on hardware which is NOT licensed by Dream Multimedia GmbH.
-#
-# If you want to use or modify the code or parts of it,
-# you have to keep MY license and inform me about the modifications by mail.
-#
+# j00zek: this file is just to avoid GS-es, when msn weather plugin not installed
+# 
 
 import time
 from Source import Source
-from Components.BlackHarmonyWeatherMSN import weathermsn
+    
+class fakeSourcesMSNWaether(Source):
 
-class BlackHarmonyMSNWeather(Source):
+	def __init__(self): Source.__init__(self)
+	def callbackAllIconsDownloaded(self): pass
+	def getCity(self): return _("n/a")
+	def getThingSpeakItems(self): return {}
+	def getWebhourlyItems(self): return {}
+	def getWebDailyItems(self): return {}
+	def getObservationPoint(self): return _("n/a")
+	def getObservationTime(self): return _("n/a")
+	def getTemperature_Heigh(self, key): return _("n/a")
+	def getTemperature_Low(self, key): return _("n/a")
+	def getTemperature_Heigh_Low(self, key): return _("n/a")		
+	def getTemperature_Text(self, key): return _("n/a")
+	def getTemperature_Current(self): return _("n/a")
+	def getFeelslike(self): return _("n/a")
+	def getHumidity(self): return _("n/a")
+	def getWinddisplay(self): return _("n/a")
+	def getWeekday(self, key, short): return _("n/a")
+	def getDate(self, key): return _("n/a")
+	def getWeatherIconFilename(self, key): return ""
+	def getCode(self, key): return ""
+	def destroy(self): Source.destroy(self)
 
-	def __init__(self):
-		Source.__init__(self)
-		weathermsn.callbacksAllIconsDownloaded.append(self.callbackAllIconsDownloaded)
-		weathermsn.getData()
-
-	def callbackAllIconsDownloaded(self):
-		self.changed((self.CHANGED_ALL,))
-	
-	def getCity(self):
-		return weathermsn.weatherData.city
-		
-	def getObservationPoint(self):
-		skey = "-1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			return weathermsn.weatherData.weatherItems[skey].observationpoint
-		else:
-			return _("n/a")
-		
-	def getObservationTime(self):
-		skey = "-1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			if item.observationtime != "":
-				c =  time.strptime(item.observationtime, "%H:%M:%S")
-				return  time.strftime("%H:%M",c)
-			else:
-				return _("n/a")
-		else:
-			return _("n/a")
-		
-	def getTemperature_Heigh(self, key):
-		skey = str(key)
-		if skey == "-1":
-			skey = "1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			highTemp = item.high
-			return "%s°%s" % (highTemp, weathermsn.weatherData.degreetype)
-		else:
-			return _("n/a")
-			
-	
-	def getTemperature_Low(self, key):
-		skey = str(key)
-		if skey == "-1":
-			skey = "1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			lowTemp = item.low
-			return "%s°%s" % (lowTemp, weathermsn.weatherData.degreetype)
-		else:
-			return _("n/a")
-			
-	def getTemperature_Heigh_Low(self, key):
-		skey = str(key)
-		if skey == "-1":
-			skey = "1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			highTemp = item.high
-			high =  "%s°%s" % (highTemp, weathermsn.weatherData.degreetype)
-			low = self.getTemperature_Low(key)
-			return "%s - %s" % (high, low)
-		else:
-			return _("n/a")		
-	
-	
-	
-	def getTemperature_Text(self, key):
-		skey = str(key)
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			if skey == "-1":
-				return item.skytext
-			else:
-				return  item.skytextday
-		else:
-			return _("n/a")
-			
-	def getTemperature_Current(self):
-		skey = "-1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			return "%s°%s" % (weathermsn.weatherData.weatherItems[skey].temperature, weathermsn.weatherData.degreetype)
-		else:
-			return _("n/a")
-			
-	def getFeelslike(self):
-		skey = "-1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			return weathermsn.weatherData.weatherItems[skey].feelslike
-		else:
-			return _("n/a")
-	
-	def getHumidity(self):
-		skey = "-1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			return weathermsn.weatherData.weatherItems[skey].humidity
-		else:
-			return _("n/a")
-			
-	def getWinddisplay(self):
-		skey = "-1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			return weathermsn.weatherData.weatherItems[skey].winddisplay
-		else:
-			return _("n/a")
-	
-	def getWeekday(self, key, short):
-		skey = str(key)
-		if skey == "-1":
-			skey = "1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			if short:
-				return item.shortday
-			else:
-				return item.day
-		else:
-			return _("n/a")
-			
-	def getDate(self, key):
-		skey = str(key)
-		if skey == "-1":
-			skey = "1"
-		if weathermsn.weatherData.weatherItems.has_key(skey):
-			item = weathermsn.weatherData.weatherItems[skey]
-			c = time.strptime(item.date,"%Y-%m-%d")
-			return time.strftime("%d. %b",c)
-		else:
-			return _("n/a")
-			
-	def getWeatherIconFilename(self, key):
-		if weathermsn.weatherData.weatherItems.has_key(str(key)):
-			return weathermsn.weatherData.weatherItems[str(key)].iconFilename
-		else:
-			return ""
-			
-	def getCode(self, key):
-		if weathermsn.weatherData.weatherItems.has_key(str(key)):
-			return weathermsn.weatherData.weatherItems[str(key)].code
-		else:
-			return ""
-
-	def destroy(self):
-		weathermsn.callbacksAllIconsDownloaded.remove(self.callbackAllIconsDownloaded)
-		Source.destroy(self)
+try:
+    from Components.Sources.MSNWeather import MSNWeather as BlackHarmonyMSNWeather
+except Exception:
+    BlackHarmonyMSNWeather = fakeSourcesMSNWaether 
