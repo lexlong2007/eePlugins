30b59f6e184edcc46c194cceef256045b199d245
Zdzislaw <zdzislaw22@windowslive.com>
2019-01-28 07:54:24 +0100

    poprawki

--- UserSkin/scripts/reportGS.sh
+++ UserSkin/scripts/reportGS.sh
@@ -1,41 +1,40 @@
 #/usr/bin/bash
-timePeriod=1
-
-sleep 5
+echo "############################################" >> /tmp/enigma2_cleanUp.log
+echo "reportGS.sh initiated" >> /tmp/enigma2_cleanUp.log
+echo "############################################" >> /tmp/enigma2_cleanUp.log
+timePeriodOnSeconds=60
+DeleteLogAfterSeconds=259200
 
 currSkin=`cat /etc/enigma2/settings|grep 'config.skin.primary_skin='|cut -d '=' -f2|cut -d '/' -f1`
 SkinModsPath="/usr/share/enigma2/$currSkin/mySkin"
 
-currEPOC=`date +%s`
+currEPOC=`date +%s` #returns EPOC in seconds
 echo curr EPOC=$currEPOC
 lastGScount=0
 GScont=0
-for filename in `ls /hdd/dvbapp2_crash*.log`
+for filename in `ls /hdd/dvbapp2_crash*.log 2>/dev/null`
 do
     fileEPOC=`date -r $filename +%s`
     diffEPOC=$((currEPOC - fileEPOC))
-    diffEPOC=$((diffEPOC / 60))
     GScont=$((GScont+1))
-    if [ $diffEPOC -lt $timePeriod ];then
+    if [ $diffEPOC -lt $timePeriodOnSeconds ];then
         lastGScount=$((lastGScount+1))
     fi
-    #echo $filename created $diffEPOC minutes ago, skipping
-    cat $filename|sed -n '/^Traceback/,/^[\*]*KERNEL LOG/p' > $filename.Traceback
+    if [ $diffEPOC -lt $DeleteLogAfterSeconds ];then
+        cat $filename|sed -n '/^Traceback/,/^[\*]*KERNEL LOG/p' > $filename.Traceback
+    fi
     rm -f $filename
-    #if [ `cat $filename|grep -c 'File*UserSkin'` -gt 0 ];then
-    #    echo aqq
-    #fi
 done
-#if [ `cat /etc/enigma2/settings|grep -c 'config.plugins.UserSkin.ReportGS'` -ge 1 ];then
 
 if [ $lastGScount -gt 1 ];then
-    echo "Analyzed $GScont GS logs." >> /tmp/safeMode.log
+    echo "Analyzed $GScont GS logs." >> /tmp/enigma2_cleanUp.log
     echo "Multiple GS's ($lastGScount) in short time detected !!!"
     echo "removing skin parts configuration"
     [ -e $SkinModsPath ] && rm -f $SkinModsPath/*.xml
-    echo "Multiple GS's in short time detected, skin parts configuration has been removed!!!" >> /tmp/safeMode.log
+    [ -e /etc/enigma2/skin_user_BlackHarmony.xml ] && rm -f /etc/enigma2/skin_user_BlackHarmony.xml
+    [ -e /usr/share/enigma2/BlackHarmony/mySkin/skin_user_BlackHarmony.xml ] && rm -f /usr/share/enigma2/BlackHarmony/mySkin/skin_user_BlackHarmony.xml
+    echo "Multiple GS's in short time detected, skin parts configuration has been deleted!!!" >> /tmp/enigma2_cleanUp.log
 else
     echo "Analyzed $GScont GS logs. No issues found. :)"
-    echo "Analyzed $GScont GS logs. No issues found. :)" >> /tmp/safeMode.log
+    echo "Analyzed $GScont GS logs. No issues found. :)" >> /tmp/enigma2_cleanUp.log
 fi
-
