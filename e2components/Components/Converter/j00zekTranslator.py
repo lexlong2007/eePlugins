749003d47da802f44e90ca2e00d968bbfb1f61fb
Zdzislaw <zdzislaw22@windowslive.com>
2019-12-15 07:57:48 +0100

    zmiany w e2c

--- e2components/Components/Converter/j00zekTranslator.py
+++ e2components/Components/Converter/j00zekTranslator.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
-#  
+#
+# j00zek 2018/2019
+#
+
 from Components.Converter.Converter import Converter
 from Components.Element import cached
 from Components.Language import language
 
 #simple and dirty translation
 if language.getLanguage() == 'pl_PL':
-    translationsDict = {'Support for skin:': 'Wsparcie:', 'http://forum.dvhk.pl': 'http://forum.dvhk.to', 'Flash Free:': 'Wolne:',
+    translationsDict = {'Support for skin:': 'Wsparcie:', 'Flash Free:': 'Wolne:',
                         'Load Avg:': 'Obciążenie CPU:', 'Uptime:': 'Czas pracy:', 'Memory:': 'Pamięć RAM:', 'Box Type:': 'Typ tunera:', 'Flash:': 'Pamięć Flash:',
-                        'Flash Free:': 'Wolne:'}
+                        'Flash Free:': 'Wolne:', 'Show Maps': 'Mapy',
+                        'Show Histograms': 'Histogramy'}
 else:
     translationsDict = {} 
     
 class j00zekTranslator(Converter, object):
     def __init__(self, LabelText):
         Converter.__init__(self, LabelText)
-        self.translatedLabel = translationsDict.get(LabelText, LabelText)
+        LabelText = _(LabelText) #first translation using enigma2.po
+        self.translatedLabel = translationsDict.get(LabelText, LabelText) #second own translation, if exists
 
     @cached
     def getText(self):
