3c9ef1e6bf53435cb613316f20b8a6182cb180a2
Zdzislaw <zdzislaw22@windowslive.com>
2019-12-15 06:44:51 +0100

    zmiany w shoutcast by kos

---
+++ zmodowanePrzezKos/SHOUTcast/__init__.py
@@ -0,0 +1,17 @@
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+import gettext
+
+def localeInit():
+    gettext.bindtextdomain('SHOUTcast', resolveFilename(SCOPE_PLUGINS, 'Extensions/SHOUTcast/locale'))
+
+
+def _(txt):
+    t = gettext.dgettext('SHOUTcast', txt)
+    if t == txt:
+        t = gettext.gettext(txt)
+    return t
+
+
+localeInit()
+language.addCallback(localeInit)
