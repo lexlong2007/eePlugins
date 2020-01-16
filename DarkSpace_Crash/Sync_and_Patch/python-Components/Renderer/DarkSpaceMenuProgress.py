a03eddb29919e96dd721a099e6baee9927da4df1
Zdzislaw <zdzislaw22@windowslive.com>
2020-01-16 18:34:28 +0100

    off3

--- DarkSpace_Crash/Sync_and_Patch/python-Components/Renderer/DarkSpaceMenuProgress.py
+++
@@ -1,3 +0,0 @@
-# j00zek: this file has changed name just to avoid errors using opkg (situation when file was installed by different package)
-#
-from Plugins.Extensions.FluidNextSetup.fluidnext import MenuProgress as DarkSpaceMenuProgress
