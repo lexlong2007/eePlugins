26a73be924b99d766ebe91f52db9e2bc47b3f1d2
Zdzislaw <zdzislaw22@windowslive.com>
2020-02-24 19:00:14 +0100

    clean bh

--- BH/scripts/cleanUp.sh
+++ BH/scripts/cleanUp.sh
@@ -3,4 +3,5 @@ find /DuckboxDisk/github/eePlugins/BH/usr/share/enigma2 -name "*.xml" |
 while read F 
 do
   sed -i 's/type="EventName"/type="j00zekModEventName"/g' "$F"
+  sed -i 's/type="ABTCAirlyWidget"/type="BlackHarmonyABTCAirlyWidget"/g' "$F"
 done
