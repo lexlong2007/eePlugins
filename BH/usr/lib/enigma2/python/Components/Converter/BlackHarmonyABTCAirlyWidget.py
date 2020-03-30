1c41fe440ccfcf5e5623d3715f254601fa1dd3ee
Zdzislaw <zdzislaw22@windowslive.com>
2020-03-30 17:07:05 +0200

    usunięcie rendererów ze skórki

--- BH/usr/lib/enigma2/python/Components/Converter/BlackHarmonyABTCAirlyWidget.py
+++
@@ -1,4 +0,0 @@
-try:
-    from Components.Converter.ABTCAirlyWidget import ABTCAirlyWidget as BlackHarmonyABTCAirlyWidget
-except Exception:
-    from Components.Converter.j00zekMissingConverter import j00zekMissingConverter as BlackHarmonyABTCAirlyWidget
