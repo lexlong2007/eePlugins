7fd83b3fa792a425f31b04b9416d0c5c70cb0370
Zdzislaw <zdzislaw22@windowslive.com>
2020-03-31 07:31:15 +0200

    poprawka msnweather

--- MSNweather-NP/Plugins/Extensions/MSNweather/plugin.py
+++ MSNweather-NP/Plugins/Extensions/MSNweather/plugin.py
@@ -97,7 +97,12 @@ def main(session,**kwargs):
     session.open(MSNweather)
 
 def sessionstart(session, **kwargs):
-	session.screen["MSNWeather"] = getWeather()
+    session.screen["MSNWeather"] = getWeather()
+    try:
+        from Components.Sources.MSNWeatherNP import MSNWeatherNP
+        session.screen['MSNWeatherNP'] = MSNWeatherNP()
+    except Exception as e:
+        printDEBUG("sessionstart Exception: %s" % str(e))
 
 def Plugins(**kwargs):
     list = [
