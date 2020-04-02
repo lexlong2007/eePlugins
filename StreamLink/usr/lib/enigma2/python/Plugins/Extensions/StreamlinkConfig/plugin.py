13e02f6c8a0af50e68cc997cf54e06f3c213fb1c
Zdzislaw <zdzislaw22@windowslive.com>
2020-04-02 06:50:37 +0200

    poprawka streamlink

--- StreamLink/usr/lib/enigma2/python/Plugins/Extensions/StreamlinkConfig/plugin.py
+++ StreamLink/usr/lib/enigma2/python/Plugins/Extensions/StreamlinkConfig/plugin.py
@@ -1,12 +1,9 @@
 from Plugins.Plugin import PluginDescriptor
 from . import mygettext as _
-from StreamlinkConfiguration import StreamlinkConfiguration
 
 def main(session, **kwargs):
-    if menuid == "vtimain" or menuid == "system":
-        return [(_("Streamlink Configuration"), main, "StreamlinkConfiguration", 40)]
-    return []
+    from StreamlinkConfiguration import StreamlinkConfiguration
+    session.open(StreamlinkConfiguration)
 
 def Plugins(path, **kwargs):
-    #return [PluginDescriptor(name=_("Streamlink Configuration"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main, needsRestart = False)]
-    return [PluginDescriptor(name=_("Streamlink Configuration"), where = PluginDescriptor.WHERE_MENU, fnc = main, needsRestart = False)]
+    return [PluginDescriptor(name=_("Streamlink Configuration"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main, needsRestart = False)]
